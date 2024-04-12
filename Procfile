4bcea80addf4b968aebadb3a2f1bce8eb83d40e9
Arnaz De Jesus <apdejesus@hau.edu.ph>
2024-04-12 14:33:44 +0800

    Celery changed schedule

--- Procfile
+++ Procfile
@@ -1,1 +1,3 @@
 web: gunicorn backend.wsgi
+worker: celery -A backend worker -l info --pool=solo
+beat: celery -A backend beat -l INFO
--- backend/celery.py
+++ backend/celery.py
@@ -14,6 +14,6 @@ app.autodiscover_tasks()
 app.conf.beat_schedule = {
     'check_subscription_expiration': {
         'task': 'profiles.tasks.check_subscription_expiration',
-        'schedule': crontab(hour=14, minute=30),
+        'schedule': crontab(hour=14, minute=36),
     },
 }
